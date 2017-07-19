717af506b731e37c6e9120af6e3746fc4a2a5ab6
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-18 18:43:07 -0700

    newly registered users get added to database

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 1 or len(postData['last_name']) < 1:
+            errors['name'] = 'First and last names cannot be blank!'
+        
+        if len(postData['email']) < 1:
+            errors['email'] = 'Email cannot be blank!'
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Invalid email address!'
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = BlogManager()
+    def __repr__(self):
+        return "<User object: {} {}>".format(self.first_name, self.last_name)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
+from django.core.urlresolvers import reverse
+
+from .models import User
 
 def index(request):
-    return HttpResponse('hello world')
+    users = User.objects.all()
+    context = {"users": []}
+    for user in users:
+        context['users'].append({
+            "id": user.id,
+            "full_name": user.first_name + " " + user.last_name,
+            "email": user.email,
+            "created_at": user.created_at.strftime('%B %e, %Y')
+        })
+    return render(request, 'users/index.html', context)
+
+def show(request, id):
+    if request.method == 'GET':
+        id = int(id)
+        user = User.objects.get(id=id)
+        context = {
+            "id": id,
+            "full_name": user.first_name + " " + user.last_name,
+            "email": user.email,
+            "created_at": user.created_at.strftime('%B %e, %Y')
+        }
+        return render(request, 'users/user.html', context)
+    elif request.method == 'POST':
+        pass
+
+def new(request):
+    return render(request, 'users/new.html')
+
+def create(request):
+    if request.method == 'POST':
+
+        # FORM VALIDATION
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect(reverse('users:new'))
+
+        # ADD TO DATABASE
+        User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'])
+        new_user_id = str(User.objects.last().id)
+        
+        '''ASK KEVIN ABOUT HOW TO REDIRECT HERE'''
+        return redirect('/users/' + new_user_id)
+    else:
+        return redirect(reverse('users:index'))
